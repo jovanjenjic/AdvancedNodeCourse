ee286c482a33ca481c461db8965416e8dbd3a444
jovanjenjic <57072437+jovanjenjic@users.noreply.github.com>
2022-01-13 18:40:20 +0100

    First commit

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://JovanJenjic:JovanJenjic123!@traversymedia-shard-00-00.j0h3o.mongodb.net:27017,traversymedia-shard-00-01.j0h3o.mongodb.net:27017,traversymedia-shard-00-02.j0h3o.mongodb.net:27017/NodeAdvancedCourse?ssl=true&replicaSet=atlas-3uckyx-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
